a00809642d6e351c382de104a599dffb604d8ad9
LightningMods <34066913+LightningMods@users.noreply.github.com>
2020-08-04 00:44:50 -0400

    Add files via upload

---
+++ source/defines.h
@@ -0,0 +1,96 @@
+
+#define KERN_DUMPSIZE 		108806144
+#define PAGE_SIZE 16348
+#define KERN_DUMPITER KERN_DUMPSIZE / PAGE_SIZE 	// can only dump a page at at time so we need to iterate
+
+#define	CTL_KERN	1				/* "high kernel": proc, limits */
+#define	KERN_PROC	14				/* struct: process entries */
+#define	KERN_PROC_VMMAP	32				/* VM map entries for process */
+#define	KERN_PROC_PID	1				/* by process id */
+
+
+struct auditinfo_addr {
+    /*
+    4    ai_auid;
+    8    ai_mask;
+    24    ai_termid;
+    4    ai_asid;
+    8    ai_flags;r
+    */
+    char useless[184];
+};
+
+unsigned int long long __readmsr(unsigned long __register) {
+	// Loads the contents of a 64-bit model specific register (MSR) specified in
+	// the ECX register into registers EDX:EAX. The EDX register is loaded with
+	// the high-order 32 bits of the MSR and the EAX register is loaded with the
+	// low-order 32 bits. If less than 64 bits are implemented in the MSR being
+	// read, the values returned to EDX:EAX in unimplemented bit locations are
+	// undefined.
+	unsigned long __edx;
+	unsigned long __eax;
+	__asm__ ("rdmsr" : "=d"(__edx), "=a"(__eax) : "c"(__register));
+	return (((unsigned int long long)__edx) << 32) | (unsigned int long long)__eax;
+}
+
+#define X86_CR0_WP (1 << 16)
+
+static inline __attribute__((always_inline)) uint64_t readCr0(void) {
+	uint64_t cr0;
+	
+	asm volatile (
+		"movq %0, %%cr0"
+		: "=r" (cr0)
+		: : "memory"
+ 	);
+	
+	return cr0;
+}
+
+static inline __attribute__((always_inline)) void writeCr0(uint64_t cr0) {
+	asm volatile (
+		"movq %%cr0, %0"
+		: : "r" (cr0)
+		: "memory"
+	);
+}
+
+
+struct ucred {
+			uint32_t useless1;
+			uint32_t cr_uid;     // effective user id
+			uint32_t cr_ruid;    // real user id
+ 			uint32_t useless2;
+    	uint32_t useless3;
+    	uint32_t cr_rgid;    // real group id
+    	uint32_t useless4;
+    	void *useless5;
+    	void *useless6;
+    	void *cr_prison;     // jail(2)
+    	void *useless7;
+    	uint32_t useless8;
+    	void *useless9[2];
+    	void *useless10;
+    	struct auditinfo_addr useless11;
+    	uint32_t *cr_groups; // groups
+    	uint32_t useless12;
+};
+
+struct filedesc {
+	void *useless1[3];
+    	void *fd_rdir;
+    	void *fd_jdir;
+};
+
+struct proc {
+    	char useless[64];
+    	struct ucred *p_ucred;
+    	struct filedesc *p_fd;
+};
+
+struct thread {
+    	void *useless;
+    	struct proc *td_proc;
+};
+
+ 
---
+++ source/main.c
@@ -0,0 +1,154 @@
+  
+/*****************************************************************
+*
+* ============== Kernel Dumper for PS4 - WildCard && LM ===============
+*
+*	Support for 6.20
+*
+*	Thanks to:
+*	-Qwertyuiop for his kernel exploits
+* -Specter for his Code Execution method
+*	-IDC for helping to understand things
+*	-Shadow for the copyout trick ;)
+*       -ChendoChap for the 6.20 exploit etc
+*
+******************************************************************/
+#include "ps4.h"
+#include "defines.h"
+
+
+uint64_t uaddr;
+void* kbase;
+uint64_t kaddr;
+
+int kdump(struct thread *td){
+
+	
+
+	int (*printfkernel)(const char *fmt, ...) = (void *)(kbase + 0x00307E10);
+	int (*copyout)(const void *kaddr, void *uaddr, size_t len) = (void *)(kbase + 0x00114800);
+	void (*bzero)(void *b, size_t len) = (void *)(kbase + 0x00114640);
+
+
+
+	// run copyout into userland memory for the kaddr we specify
+	int cpRet = copyout(kaddr, uaddr , PAGE_SIZE);
+
+	// if mapping doesnt exist zero out that mem
+	if(cpRet == -1){
+		printfkernel("bzero at 0x%016llx\n", kaddr);
+		bzero(uaddr, PAGE_SIZE);
+		return cpRet;
+	}
+	
+	return cpRet;
+}
+
+int kpayload(struct thread *td){
+
+	struct ucred* cred;
+	struct filedesc* fd;
+
+	fd = td->td_proc->p_fd;
+	cred = td->td_proc->p_ucred;
+
+
+	void* kernel_base = &((uint8_t*)__readmsr(0xC0000082))[-0x1C0];
+	uint8_t* kernel_ptr = (uint8_t*)kernel_base;
+	void** got_prison0 =   (void**)&kernel_ptr[0x0113D458];
+	void** got_rootvnode = (void**)&kernel_ptr[0x021C3AC0];
+
+         kbase=kernel_base;
+
+	// resolve kernel functions
+
+	int (*printfkernel)(const char *fmt, ...) = (void *)(kernel_base + 0x00307E10);
+
+	cred->cr_uid = 0;
+	cred->cr_ruid = 0;
+	cred->cr_rgid = 0;
+	cred->cr_groups[0] = 0;
+
+	cred->cr_prison = *got_prison0;
+	fd->fd_rdir = fd->fd_jdir = *got_rootvnode;
+
+	// escalate ucred privs, needed for access to the filesystem ie* mounting & decrypting files
+	void *td_ucred = *(void **)(((char *)td) + 304); // p_ucred == td_ucred
+	
+	// sceSblACMgrIsSystemUcred
+	uint64_t *sonyCred = (uint64_t *)(((char *)td_ucred) + 96);
+	*sonyCred = 0xffffffffffffffff;
+	
+	// sceSblACMgrGetDeviceAccessType
+	uint64_t *sceProcType = (uint64_t *)(((char *)td_ucred) + 88);
+	*sceProcType = 0x3801000000000013; // Max access
+	
+	// sceSblACMgrHasSceProcessCapability
+	uint64_t *sceProcCap = (uint64_t *)(((char *)td_ucred) + 104);
+	*sceProcCap = 0xffffffffffffffff; // Sce Process
+
+	// Say hello and put the kernel base in userland to we can use later
+
+	printfkernel("\n\n\nHELLO FROM YOUR KERN DUDE =)\n\n\n");
+
+	printfkernel("kernel base is:0x%016llx\n", kernel_base);
+
+	printfkernel("uaddr is:0x%016llx\n", uaddr);
+
+	return 0;
+}
+
+
+
+int _main(struct thread *td){
+
+	// Init and resolve libraries
+	initKernel();
+	initLibc();
+	initNetwork();
+	initPthread();
+
+	uint64_t* dump = mmap(NULL, PAGE_SIZE, PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_PRIVATE, -1, 0);
+	uint64_t filedump = mmap(NULL, KERN_DUMPSIZE, PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_PRIVATE, -1, 0);
+
+	// patch some things in the kernel (sandbox, prison, debug settings etc..)
+	
+
+        uaddr = dump;
+
+	syscall(11,&kpayload);
+
+
+	
+	uint64_t pos = 0;
+
+
+	// loop enough to dump up until gpu used memory
+	for(int i = 0; i < KERN_DUMPITER; i++){
+	
+ 		kaddr = kbase + pos;
+
+
+		uaddr = filedump + pos;
+
+
+		syscall(11,&kdump);
+
+
+		pos = pos + PAGE_SIZE;
+	}
+
+
+
+	// write to file		
+	int fd = open("/mnt/usb0/Kernel_Dump_620.bin", O_WRONLY | O_CREAT | O_TRUNC, 0777);
+
+	write(fd, filedump, KERN_DUMPSIZE); // Write the userland buffer to USB
+
+        close(fd);
+
+	munmap(dump, PAGE_SIZE);
+	munmap(filedump, KERN_DUMPSIZE);
+
+	return 0;
+}
